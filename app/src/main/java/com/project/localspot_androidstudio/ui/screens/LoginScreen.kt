513c4da264aeadcc677d50a6897996c52d2448f6
Yiqun2000 <lin.yiqun@alu.ceacfp.es>
2025-10-27 16:44:09 +0100

    1r commit StartScreen

--- app/src/main/java/com/project/localspot_androidstudio/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/project/localspot_androidstudio/ui/screens/LoginScreen.kt
@@ -2,9 +2,11 @@ package com.project.localspot_androidstudio.ui.screens
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import com.project.localspot_androidstudio.ui.components.PrimaryButton
 
 
+@Preview
 @Composable
 fun LoginScreen () {
 
