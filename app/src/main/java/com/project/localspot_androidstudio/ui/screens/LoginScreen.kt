0b301b765f6eef991cd34bccce5c7288433816cb
Yiqun2000 <lin.yiqun@alu.ceacfp.es>
2025-10-27 14:12:31 +0100

    LoginScreen + components

---
+++ app/src/main/java/com/project/localspot_androidstudio/ui/screens/LoginScreen.kt
@@ -0,0 +1,12 @@
+package com.project.localspot_androidstudio.ui.screens
+
+import androidx.compose.runtime.Composable
+import com.project.localspot_androidstudio.ui.components.PrimaryButton
+
+
+@Composable
+fun LoginScreen () {
+
+    PrimaryButton()
+
+}
