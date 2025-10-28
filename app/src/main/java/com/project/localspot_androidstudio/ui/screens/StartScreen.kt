140575dc7df2fe5f5b4dfa73393622834f84473d
Yiqun2000 <lin.yiqun@alu.ceacfp.es>
2025-10-28 08:45:46 +0100

    Arreglo de Packages

--- app/src/main/java/com/project/localspot_androidstudio/ui/screens/StartScreen.kt
+++ app/src/main/java/com/project/localspot_androidstudio/ui/screens/StartScreen.kt
@@ -1,9 +1,33 @@
 package com.project.localspot_androidstudio.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 
 @Composable
-fun StartScreen () {
-    Text("Hola")
+fun StartScreen() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "LocalSpot",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            modifier = Modifier.padding(top = 16.dp)
+        )
+    }
 }
